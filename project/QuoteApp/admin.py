927eb415bd099c095ef9ca4ce4934c90449c7b9d
Sanatan kumar <sanatankumarchaudhary@gmail.com>
2017-03-22 06:23:24 +0530

    Added many things (almost complete)

--- project/QuoteApp/admin.py
+++ project/QuoteApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Quotes
 # Register your models here.
+class QuotesAdmin(admin.ModelAdmin):
+    model = Quotes
+    list_display = ('get_quotes', 'author', 'user')
+
+admin.site.register(Quotes, QuotesAdmin)
